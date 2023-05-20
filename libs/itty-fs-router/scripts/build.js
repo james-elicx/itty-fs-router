54ecf6b27b7e3a0cd4457956bfbe0372e31972de
James <james@eli.cx>
2023-05-21 00:14:15 +0100

    fix: build issue

--- libs/itty-fs-router/scripts/build.js
+++ libs/itty-fs-router/scripts/build.js
@@ -7,3 +7,12 @@ writeFileSync(resolve('dist', 'package.json'), withoutDist);
 
 copyFileSync(resolve('README.md'), resolve('dist', 'README.md'));
 copyFileSync(resolve('..', '..', 'LICENSE.md'), resolve('dist', 'LICENSE.md'));
+
+const cliJs = readFileSync(resolve('dist', 'cli', 'index.js'), 'utf-8');
+const cliCjs = readFileSync(resolve('dist', 'cli', 'index.cjs'), 'utf-8');
+
+const cliJsFixed = `#!/usr/bin/env node\n${cliJs}`;
+const cliCjsFixed = `#!/usr/bin/env node\n${cliCjs}`;
+
+writeFileSync(resolve('dist', 'cli', 'index.js'), cliJsFixed);
+writeFileSync(resolve('dist', 'cli', 'index.cjs'), cliCjsFixed);
